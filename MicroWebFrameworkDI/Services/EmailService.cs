959a575c96433a9e9b1c5e87ccf577a41a76dde1
Niloo Mont <niloo.mon@gmail.com>
2024-02-01 00:00:11 +0330

    Reapply "added InotificationService contracts and two implementations"
    This reverts commit 26485cf5e96826a8da80117b1d3e5e8ddc48df70.

---
+++ MicroWebFrameworkDI/Services/EmailService.cs
@@ -0,0 +1,11 @@
+﻿using MicroWebFramework.Contracts;
+
+namespace MicroWebFramework.Services;
+
+public class EmailService : INotificationService
+{
+    public void Send(string destination, string message)
+    {
+        $"Sending email to email address: {destination}".Dump();
+    }
+}
---
+++ MicroWebFrameworkDI/Services/SMSService.cs
@@ -0,0 +1,11 @@
+﻿using MicroWebFramework.Contracts;
+
+namespace MicroWebFramework.Services;
+
+public class SMSService : INotificationService
+{
+    public void Send(string destination, string message)
+    {
+        $"Sending sms to phone number: {destination}".Dump();
+    }
+}

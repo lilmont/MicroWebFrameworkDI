5f4390f64f3b5f662d7680a43b5eaadd21e262f4
Niloo Mont <niloo.mon@gmail.com>
2024-02-01 00:01:46 +0330

    Reapply "Created UsersController and moved GetUserById to UsersController"
    This reverts commit 875236a56bcdc3eeec023f7bf528557f03a01cbc.

--- MicroWebFrameworkDI/Entities/User.cs
+++ MicroWebFrameworkDI/Entities/User.cs
@@ -3,4 +3,6 @@ public class User
 {
     public required int Id { get; set; }
     public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string PhoneNumber { get; set; }
 }
